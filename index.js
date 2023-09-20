87e45622b94ad2ccde38465286948a2b662f0a10
Dragunio <kdragon113@gmail.com>
2023-09-20 14:29:32 +0700

    setup test chat with socket io

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('user connected');
+    // socket.broadcast.emit('hi');
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
